9f270ae6e138f8cb9e245fa17a45bdee6944f5f3
axelxav <axel.napitupulu@gmail.com>
2024-08-09 17:40:53 +0700

    adding some section on add patient data details

--- IndeksPasien.xaml.cs
+++ IndeksPasien.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System.Windows.Controls;
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace RekamMedisPuskesmas
 {
     public partial class IndeksPasien : Page
     {
         private MainWindow _mainWindow;
+        private string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=1234;Database=puskesmas";
 
         public IndeksPasien()
         {
             InitializeComponent();
+            LoadPatientData();
         }
 
         public IndeksPasien(MainWindow mainWindow) : this()
@@ -27,5 +33,37 @@ namespace RekamMedisPuskesmas
         {
             _mainWindow.NavigateToPage(new LoginPage(_mainWindow));
         }
+
+        private void LoadPatientData()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM data_pasien";
+
+                    using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, connection))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            PatientDataGrid.ItemsSource = dataTable.DefaultView;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No data found in the table.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading data: {ex.Message}");
+                }
+            }
+        }
+
     }
 }
